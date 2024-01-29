7bf33848e66fbe859543772d07815a1e9d94ec1a
DForteaP <dforteap@gmail.com>
2024-01-29 12:06:52 +0100

    Proyecto inicial

---
+++ TrabajoDetectivesco/Source/Practica3/Actors/AInspectable.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AInspectable.generated.h"
+
+UCLASS()
+class PRACTICA3_API AAInspectable : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAInspectable();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
---
+++ TrabajoDetectivesco/Source/Practica3/Practica3GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Practica3GameMode.generated.h"
+
+UCLASS(minimalapi)
+class APractica3GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APractica3GameMode();
+};
+
+
+
